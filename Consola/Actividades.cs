5e6b0177cbe0700cffafda8a10502356668bd783
Daiana Natali Sepúlveda <daianas@users.noreply.github.com>
2021-03-21 11:46:30 -0300

    Creación repositorio consola + 4 primeras actividades

---
+++ Consola/Actividades.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadesConsola
+{
+    public class Actividades
+    {
+        public void A1()
+        {
+            Console.WriteLine("Hola mundo!");
+        }
+
+        public void A2()
+        {
+            Console.WriteLine("Ingrese texto: ");
+            string Texto = Console.ReadLine();
+            Console.WriteLine("Usted ingresó: " + Texto);
+        }
+
+        public void A3()
+        {
+            ConsoleKeyInfo Tecla;
+            do
+            {
+                Console.WriteLine("Presione la tecla X para salir");
+                Tecla = Console.ReadKey(true);
+            }
+            while (Tecla.Key != ConsoleKey.X);
+        }
+
+        public void A4()
+        {
+            string Frase;
+            do
+            {
+                Console.WriteLine("Ingrese una frase: ");
+                Frase = Console.ReadLine();
+            } while (Frase != "fin");
+        }
+
+        public void A5()
+        {
+            ConsoleKeyInfo Tecla;
+
+            do
+            {
+                Console.WriteLine("Presione Ctrl + F para salir");
+                Tecla = Console.ReadKey(true);
+            } while (Tecla.Modifiers != ConsoleModifiers.Control && Tecla.Key != ConsoleKey.F);
+
+        }
+    }
+}
